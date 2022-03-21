eb85d1d4f968686321488ae58bc452e3e0b75b89
adrv <ad.rodriguez@students.salle.url.edu>
2022-03-21 16:17:54 -0500

    Ejercicio Rest Controller findByYear

---
+++ s009-rest/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into directions (street, postal_code, city, country) values ('Calle falsa', '28009', 'Madrid', 'Spain');
+insert into directions (street, postal_code, city, country) values ('Calle verdadera', '28009', 'Madrid', 'Spain');
+insert into manufacturers (name, num_employees, year, id_direction) values ('Ford', 2000, 1980, 1);
+insert into manufacturers (name, num_employees, year, id_direction) values ('Volkswagen', 2000, 1980, 2);
